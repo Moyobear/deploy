c69cd9994bc1757fcfa472c4e844ffce5517d2f5
moyobear <rodriguezjmm@hotmail.com>
2023-02-23 14:46:05 -0400

    modificaciones necesarias

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,10 @@ server.use(cors());
 server.use(morgan("dev"));
 // server.use(express.json());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://deploy-rust-kappa.vercel.app/"
+  ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
