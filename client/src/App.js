db7bf48c34e17a393cabaf20446c06ff335a1ebe
moyobear <rodriguezjmm@hotmail.com>
2023-02-23 15:28:58 -0400

    Nuevo backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import style from "./styles/App.module.css";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://deploy-production-d989.up.railway.app/";
 
 function App() {
   const location = useLocation();
