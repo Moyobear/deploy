e6b06f00da80fe4906b8df8a6de20ba40611b943
moyobear <rodriguezjmm@hotmail.com>
2023-02-23 15:33:41 -0400

    modificacion de la ruta del front

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://deploy-rust-kappa.vercel.app/"
+    "https://https://deploy-moyobear.vercel.app/"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
